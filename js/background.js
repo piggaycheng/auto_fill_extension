7f68ffcfd6565686482bc4341bd716d2900e05ed
piggaycheng <piggaycheng123@gmail.com>
2019-09-25 00:12:23 +0800

    add contextMenus

--- js/background.js
+++ js/background.js
@@ -2,6 +2,14 @@ chrome.runtime.onMessage.addListener(function(myMessage, sender, sendResponse){
     //do something that only the extension has privileges here
     console.log(myMessage);
     return true;
- });
+});
 
 console.log("background page ready");  
+
+// let contextMenusItem = {
+//     "id": "Auto Fill",
+//     "title": "Auto Fill",
+//     "contexts": ["all"]
+// };
+
+// chrome.contextMenus.create(contextMenusItem);
