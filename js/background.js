cfbf0fb57daaf70aab311c4f8fcefe0472f2565a
piggaycheng <piggaycheng123@gmail.com>
2019-10-10 22:57:16 +0800

    add modal in content script

--- js/background.js
+++ js/background.js
@@ -9,23 +9,31 @@ chrome.runtime.onMessage.addListener(function(myMessage, sender, sendResponse){
 let contextMenusItem = {
     "id": "autoFill",
     "title": "新增至AutoFill",
-    "contexts": ["selection"]
+    "contexts": ["all"]
 };
 
 chrome.contextMenus.create(contextMenusItem);
 
-chrome.contextMenus.onClicked.addListener(function(clickData){
-    if(clickData.menuItemId == 'autoFill' && clickData.selectionText) {
-        let input = {};
-        chrome.storage.sync.get(['input'], function(result) {
-            if(result.input) {
-                result.input[clickData.selectionText] = '';
-                input = result.input;
-            } else {
-                input[clickData.selectionText] = '';
-            }
-            console.log(input);
-            chrome.storage.sync.set({'input': input});
-        });
-    }
-});
+function onContextMenusClickHandler(clickData, tab) {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog_box"});  
+    });
+}
+
+chrome.contextMenus.onClicked.addListener(onContextMenusClickHandler);
+
+// chrome.contextMenus.onClicked.addListener(function(clickData){
+//     if(clickData.menuItemId == 'autoFill' && clickData.selectionText) {
+//         let input = {};
+//         chrome.storage.sync.get(['input'], function(result) {
+//             if(result.input) {
+//                 result.input[clickData.selectionText] = '';
+//                 input = result.input;
+//             } else {
+//                 input[clickData.selectionText] = '';
+//             }
+//             console.log(input);
+//             chrome.storage.sync.set({'input': input});
+//         });
+//     }
+// });
--- js/content_script.js
+++ js/content_script.js
@@ -1,15 +1,40 @@
 console.log("內容腳本注入");  
 
-$('body').click(function() {
-    //send message to ext
-    console.log("body click");  
-    var testInformation = 'test';
-    chrome.runtime.sendMessage(testInformation, function(response) {
-    //callback
-    });
+// $('body').click(function() {
+//     //send message to ext
+//     console.log("body click");  
+//     var testInformation = 'test';
+//     chrome.runtime.sendMessage(testInformation, function(response) {
+//     //callback
+//     });
+// });
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    if (msg.action == 'open_dialog_box') {
+        if($('#addInputModal').length == 0) {
+            $('body').append(
+                '<div class="modal" tabindex="-1" role="dialog" id="addInputModal" data-backdrop="false">'+
+                    '<div class="modal-dialog" role="document">'+
+                        '<div class="modal-content">'+
+                            '<div class="modal-header">'+
+                                '<h5 class="modal-title">Modal title</h5>'+
+                                '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
+                                    '<span aria-hidden="true">&times;</span>'+
+                                '</button>'+
+                            '</div>'+
+                            '<div class="modal-body">'+
+                                '<p>Modal body text goes here.</p>'+
+                            '</div>'+
+                            '<div class="modal-footer">'+
+                                '<button type="button" class="btn btn-primary">Save changes</button>'+
+                                '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'
+            );
+        }
+
+        $('#addInputModal').modal('toggle');
+    }
 });
-
-
-// document.body.addEventListener('click', function(){
-//     console.log("內容腳本注入");  
-// }, true);
